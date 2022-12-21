67ea4375ef1b3795d1cd75ebd9dd517fc120f041
appweb <appweb.live@gmail.com>
2022-12-21 09:57:28 +0530

    first commit

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  env: {
+    ApiKey: process.env.ApiKey,
+    AuthDomain: process.env.AuthDomain,
+    projectId: process.env.projectId,
+    Client_id:process.env.Client_id,
+  }
 }
 
 module.exports = nextConfig
